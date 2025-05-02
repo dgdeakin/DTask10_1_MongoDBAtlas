770dc3a4243fb53b19bb2258902ae484690b791c
dgdeakin <dg22241682@gmail.com>
2025-05-02 22:52:44 +1000

    M

---
+++ README.md
@@ -0,0 +1,103 @@
+
+# About the project (Informative Tutorial ONLY)
+This is a simple Flask application to show connection to MongoDB Atlas with CRUD application for notes.
+The frontend simple sample application for android is provided here:
+
+# To run both frontend and backend:
+- First run the backend. (Set up takes time like mongodb account, creating cluster etc.)
+- Android app(just run the app).
+
+# Instructions:
+## 1. First clone the repo or download.
+## 2. Run terminal in the project folder.
+## 3. Make sure python is installed. The python version tested is 3.12 (may work with other version also). Then Create virtual environment as:
+   - ```python -m venv venv```
+## 4. Activate virtual environment
+   - Windows Powershell: 
+     - ```.\venv\Scripts\Activate.ps1```
+   - Mac OS: 
+     - ```source venv\bin\activate```
+   
+## 5. Then install libraries required.
+   - pip install Flask
+   - pip install Flask-PyMongo
+   https://flask-pymongo.readthedocs.io/en/latest/
+   - pip install python-dotenv
+   https://pypi.org/project/python-dotenv/
+
+## 6. Before you can run the app, you need to setup account MongoDB (ONLY choose FREE option):
+   - First signup with MongoDB.
+   
+  <img src="screenshots/img_4.png" width=300>
+   
+   - Create organization (if not present).
+
+   <img src="screenshots/img8.png" width=500>
+
+   - Then Create project.
+
+   <img src="screenshots/img_7.png" width=500>
+
+   - Create cluster (Choose FREE Option)
+
+   <img src="screenshots/img_10.png" width=500>
+ 
+   - Deploy cluster:
+
+   <img src="screenshots/img_11.png" width=500>
+ 
+   - Then Connect to Cluster -> Create Database User -> Choose a connection method.
+
+   <img src="screenshots/img_12.png" width=500>
+
+   - Connection Method: Connect to your application -> Drivers.
+
+   <img src="screenshots/img_5.png" width=500>
+  
+   - Select driver, install driver and save your connection string:
+
+   <img src="screenshots/img_6.png" width=500>
+  
+   - Use connection string in the code. Just replace the connection string in the code:
+
+   ```app.config["MONGO_URI"] = "mongodb+srv://..."```
+
+## 7. Then, simple run in terminal:
+   - python main.py 
+
+   
+
+# Output:
+## 1. Once you run ```python main.py``` you should get output in terminal like this:
+
+   <img src="screenshots/img.png">
+   
+and in browser:
+
+<img src="screenshots/img_7.png" width=500>
+
+## 2. You can try various routes (urls) as:
+   - Just chek the connection first using /ping route as below:
+   
+<img src="screenshots/img_2.png" width="500">    
+
+   - http://127.0.0.1:5000/notes-page 
+(Run on your browser and it should output like below)
+     (NOTE: You will only see list if you already run android app or created database with input data. 
+     you can do that after running sample frontend android app provided.)
+
+
+  <img src="screenshots/img_1.png" width=500> gives
+ 
+  - http://127.0.0.1:5000/notes
+  
+  <img src="screenshots/img_3.png" width="500">
+
+##  3. If you don't see theses, just run the front end android app and add notes from there or use postman.
+
+In postman, 
+
+ <img src="screenshots/img_8.png" width="500">
+
+#References:
+https://pypi.org/project/Flask-PyMongo/
---
+++ main.py
@@ -0,0 +1,110 @@
+from flask import Flask, request, jsonify, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+# Load environment variables
+load_dotenv()
+#  ----------NOTE----------
+# Provide your MONGO_URI (Connection String) here: You get that when you first create cluster, connect to cluster etc.; looks like this:
+# mongodb+srv://<database_user_username>:<database_user_password>@cluster0.lgrhnze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+# Don't forget to add your database name that you want to be created when first running the code: Here it is -> ----|| notes_db ||------ in MONGO_URI below.
+# app.config["MONGO_URI"] = "mongodb+srv://<database_user_username>:<database_user_password>@cluster0.lgrhnze.mongodb.net/<your_database_name>?retryWrites=true&w=majority&appName=Cluster0"
+
+# ----------------------------------------------
+# Change deakintor, TCS9MPoG9IyDhI2H and notes_db from here to your database username, database user password and your database name
+app.config["MONGO_URI"] = "mongodb+srv://deakintutor:TCS9MPoG9IyDhI2H@cluster0.lgrhnze.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0"
+if not app.config["MONGO_URI"]:
+    raise ValueError("MONGO_URI environment variable not set")
+mongo = PyMongo(app)
+
+# MongoClient for ping test
+client = MongoClient(app.config["MONGO_URI"], server_api=ServerApi('1'))
+
+# For landing page
+@app.route("/", methods=["GET"])
+def welcome_page():
+    return render_template("welcome.html")
+
+# Route to confirm MongoDB Atlas connection
+@app.route("/ping", methods=["GET"])
+def ping_mongodb():
+    try:
+        client.admin.command('ping')
+        return jsonify({"message": "Successfully connected to MongoDB Atlas!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Notes page rendering template
+@app.route("/notes-page", methods=["GET"])
+def notes_page():
+    try:
+        notes = mongo.db.notes.find()
+        return render_template("index.html", notes=notes)
+    except Exception as e:
+        return jsonify({"error": f"Failed to fetch notes: {str(e)}"}), 500
+
+# Get List of Notes
+@app.route("/notes", methods=["GET"])
+def get_notes():
+    try:
+        notes = list(mongo.db.notes.find())
+        for note in notes:
+            note["_id"] = str(note["_id"])
+        return jsonify({"notes": notes}), 200
+    except Exception as e:
+        return jsonify({"error": f"Failed to fetch notes: {str(e)}"}), 500
+
+# Add a note
+@app.route("/notes", methods=["POST"])
+def create_note():
+    try:
+        note_data = {
+            "title": request.json["title"],
+            "content": request.json["content"],
+            "category": request.json["category"]
+        }
+        result = mongo.db.notes.insert_one(note_data)
+        note_data["_id"] = str(result.inserted_id)
+        return jsonify({"message": "Note added successfully", "note": note_data}), 201
+    except Exception as e:
+        return jsonify({"error": f"Failed to create note: {str(e)}"}), 400
+
+# Update a Note
+@app.route("/notes/<id>", methods=["PUT"])
+def update_note(id):
+    try:
+        note_data = {
+            "title": request.json["title"],
+            "content": request.json["content"],
+            "category": request.json["category"]
+        }
+        result = mongo.db.notes.update_one(
+            {"_id": ObjectId(id)},
+            {"$set": note_data}
+        )
+        if result.modified_count:
+            note_data["_id"] = id
+            return jsonify({"message": "Note updated successfully", "note": note_data}), 200
+        return jsonify({"error": "Note not found"}), 404
+    except Exception as e:
+        return jsonify({"error": f"Failed to update note: {str(e)}"}), 400
+
+# Delte note
+@app.route("/notes/<id>", methods=["DELETE"])
+def delete_note(id):
+    try:
+        result = mongo.db.notes.delete_one({"_id": ObjectId(id)})
+        if result.deleted_count:
+            return jsonify({"message": "Note deleted successfully"}), 200
+        return jsonify({"error": "Note not found"}), 404
+    except Exception as e:
+        return jsonify({"error": f"Failed to delete note: {str(e)}"}), 400
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
